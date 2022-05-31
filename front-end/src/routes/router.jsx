3d0039ef52a3339b6948ad13621eaccd2f4d6724
lucas <lucmaieski@gmail.com>
2022-05-31 18:55:36 -0300

    Requisito 1 em progresso..

---
+++ front-end/src/routes/router.jsx
@@ -0,0 +1,21 @@
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+    Navigate
+  } from 'react-router-dom';
+
+import LoginPage from '../pages/LoginPage';
+
+function Router() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={ <Navigate to="/login" /> } />
+            </Routes>
+      </BrowserRouter>
+    )
+}
+
+export default Router;
