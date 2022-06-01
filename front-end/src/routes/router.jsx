b1241bf7d400fc36ea2f2ec34095e263e15141fb
Arthur Moreira <arthurmcs96@gmail.com>
2022-05-31 21:30:48 -0300

    faz a seeders de users e cumpre o req 01

--- front-end/src/routes/router.jsx
+++ front-end/src/routes/router.jsx
@@ -1,21 +1,23 @@
+import React from 'react';
+
 import {
-    BrowserRouter,
-    Route,
-    Routes,
-    Navigate
-  } from 'react-router-dom';
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 
 import LoginPage from '../pages/LoginPage';
 
 function Router() {
-    return(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={ <Navigate to="/login" /> } />
-        </Routes>
-      </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={ <LoginPage /> } />
+        <Route path="/" element={ <Navigate to="/login" /> } />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
