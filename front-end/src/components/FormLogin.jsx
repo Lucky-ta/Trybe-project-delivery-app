fbae8a0f7cf1808731614bfaa96bd232e4758063
lucas <lucmaieski@gmail.com>
2022-06-01 16:28:45 -0300

    Testando

--- front-end/src/components/FormLogin.jsx
+++ front-end/src/components/FormLogin.jsx
@@ -3,20 +3,31 @@ import { Form, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default function FormLogin() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`email: ${email}`);
+    console.log(`password: ${password}`);
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Login</Form.Label>
         <Form.Control
           type="email"
           placeholder="Enter email"
           data-testid="common_login__input-email"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <Form.Label>Senha</Form.Label>
         <Form.Control
           type="password"
           placeholder="Password"
           data-testid="common_login__input-password"
+          onChange={(e) => setPassword(e.target.value)}
         />
         <Button
           variant="primary"
